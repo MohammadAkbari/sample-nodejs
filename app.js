16763e674b60b363426feebdf5f15ef6062ae96e
makbari.it <makbari.it@outlook.com>
2019-06-24 12:36:11 +0430

    log changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const os = require('os');
-console.log("Kubia server starting...");
+console.log("app server starting ...");
 var handler = function (request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
     response.writeHead(200);
