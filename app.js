3e89100d1ae395c17ed9e7b4a890a8fcf3494a94
Mohammad Akbari <makbari.it@gmail.com>
2019-07-17 08:30:15 +0430

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log("app server starting ...");
 var handler = function (request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("hostname: " + os.hostname() + "\n" + "type: " + os.type() + "\n" + "platform: " + os.platform() + "\n");
+    
+    response.end("hostname: " + os.hostname() + "\n" + 
+                 "type: " + os.type() + "\n" + 
+                 "platform: " + os.platform() + "\n"+
+                 "uptime: " + os.uptime() + "\n"+
+                 "arch: " + os.arch() + "\n"+
+                 "release: " + os.release() + "\n"+
+                 "totalmem: " + os.totalmem() + "\n"+
+                 "freemem: " + os.freemem() + "\n"+
+                 "loadavg: " + os.loadavg() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8080);
